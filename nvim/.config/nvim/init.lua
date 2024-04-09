398972563d9bdc5fe855832ed860bacb9c0a114b
rlwrnc <raymond.lawrence@tutanota.com>
2024-04-09 14:44:00 -0600

    Make todo-comments actually work

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -38,10 +38,32 @@ require('lazy').setup({
   {
     'folke/todo-comments.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = false,
+      keywords = { 
+        FIX = { icon = "#", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, },
+        TODO = { icon = ">", color = "info" },
+        HACK = { icon = "?", color = "warning" },
+        WARN = { icon = "!", color = "warning", alt = { "WARNING", "XXX" } },
+        PERF = { icon = ".", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+        NOTE = { icon = "i", color = "hint", alt = { "INFO" } },
+        TEST = { icon = "T", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+      },
+      highlight = {
+        before = "",
+        keyword = "bg",
+        after = "",
+        pattern = {
+          [[.*(KEYWORDS)\s*:]],
+          [[.*(KEYWORDS)(.*)\s*:]]
+        },
+      },
+      search = {
+        pattern = [[\b(KEYWORDS)(\(.*\))?:]]
+      },
+    }
   }
 })
-
-require('todo')
 
 local palettes = {
   tomorrow_night = {
@@ -99,8 +121,6 @@ local palettes = {
     base0F = "#d65d0e",
   }
 }
-
-require('todo')
 
 -- set up plugins
 --   mini setup
