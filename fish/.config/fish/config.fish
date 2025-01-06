6d51a7eb250058a8926785051526deebc0ffec40
rlwrnc <raymond.lawrence@tutanota.com>
2025-01-06 12:08:08 -0600

    Fix vi mode not starting

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,7 +7,7 @@ function fish_user_key_bindings
     # resetting all bindings.
 
     # The argument specifies the initial mode (insert, "default" or visual).
-    fish_vi_key_bindings --no-erase insert
+    set -g fish_key_bindings fish_vi_key_bindings
 end
 
 if status is-interactive
