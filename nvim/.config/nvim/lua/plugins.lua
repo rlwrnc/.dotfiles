0473f3863272532d7c044d16d6f6ee7360e92d14
rlwrnc <raymond.lawrence@tutanota.com>
2026-04-08 15:15:40 -0500

    Replace mini.deps with vim.pack

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,65 +1,49 @@
--- mini
-local path_package = vim.fn.stdpath('data') .. '/site'
-local mini_path = path_package .. '/pack/deps/start/mini.nvim'
-if not vim.loop.fs_stat(mini_path) then
-  vim.cmd('echo "Installing `mini.nvim`" | redraw')
-  local clone_cmd = {
-    'git', 'clone', '--filter=blob:none',
-    'https://github.com/echasnovski/mini.nvim', mini_path
-  }
-  vim.fn.system(clone_cmd)
-  vim.cmd('packadd mini.nvim | helptags ALL')
-  vim.cmd('echo "`mini.nvim` installed!" | redraw')
+function github(repo)
+  return "https://github.com/" .. repo
 end
 
-require("mini.basics").setup()
-require("mini.icons").setup()
-require("mini.statusline").setup()
-require("mini.comment").setup()
-require("mini.completion").setup()
-require("mini.cursorword").setup()
-require("mini.fuzzy").setup()
-require("mini.move").setup()
-require("mini.pairs").setup()
-require("mini.surround").setup()
-require("mini.pick").setup()
-require("mini.files").setup()
-require("mini.diff").setup()
-require("mini.deps").setup({ path = { package = path_package } })
+vim.pack.add({
+  github("nvim-mini/mini.nvim"),
+  github("sainnhe/gruvbox-material"),
+  github("neovim/nvim-lspconfig"),
+  github("NMAC427/guess-indent.nvim"),
+  github("tpope/vim-abolish"),
 
-local add = MiniDeps.add
+  {
+    src = github("nvim-treesitter/nvim-treesitter"),
+    version = "main"
+  },
+})
 
 -- colors
-add("sainnhe/gruvbox-material")
 vim.g.gruvbox_material_background = 'hard'
 vim.g.gruvbox_material_foreground = 'material'
 vim.opt.termguicolors = true
 vim.cmd.colorscheme('gruvbox-material')
 
--- lspconfig
-add("neovim/nvim-lspconfig")
+-- mini modules
+mini_modules = {
+  "basics", "icons", "statusline", "comment", "completion", "cursorword",
+  "fuzzy", "move", "pairs", "surround", "pick", "files", "diff"
+}
+
+for _, mod in ipairs(mini_modules) do
+  require("mini.".. mod).setup()
+end
+
+-- language servers
 local language_servers = { "clangd", "pyright", "ruff", "cmake" }
 for _, server in ipairs(language_servers) do
   vim.lsp.enable(server)
 end
 
 -- treesitter
-add({
-  source = 'nvim-treesitter/nvim-treesitter',
-  checkout = 'master',
-  monitor = 'main',
-  hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
-})
-
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "c", "cpp", "lua", "python" },
-  highlight = { enable = true },
-  indent = { enable = true }
+ts_filetypes = {"c", "cpp", "lua", "python"}
+require("nvim-treesitter").install(ts_filetypes)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = ts_filetypes,
+  callback = function() vim.treesitter.start() end,
 })
 
 -- auto-detect indentation
-add("NMAC427/guess-indent.nvim")
 require("guess-indent").setup({ auto_cmd = true })
-
--- gives :Subvert
-add("tpope/vim-abolish")
